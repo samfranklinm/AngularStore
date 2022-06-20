5187643f951baeef59a2faca37af55e3700fc78c
Jagan Mohan, Sam (HQP) <sam.jaganmohan@roberthalf.com>
2022-06-20 12:21:16 -0700

    learned about navigation using Route

--- AngStore/src/app/product-details/product-details.component.ts
+++ AngStore/src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
-
+  product: Product | undefined; // define product property
+  
+  constructor(private route: ActivatedRoute ) { } // injecting ActivatedRoute into constructor
+  
   ngOnInit(): void {
+    // get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap; // access the route parameters using `route.snapshot`
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // find the product that corresponds with the id provided in route
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }

c5ca0a9dc08de8b52895eb3a56d2927bcce6fde3
Jagan Mohan, Sam (HQP) <sam.jaganmohan@roberthalf.com>
2022-06-24 13:44:29 -0700

    finished cart feature and added shipping costs

--- AngStore/src/app/product-details/product-details.component.ts
+++ AngStore/src/app/product-details/product-details.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
   product: Product | undefined; // define product property
-  
-  constructor(private route: ActivatedRoute ) { } // injecting ActivatedRoute into constructor
-  
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {} // injecting ActivatedRoute into constructor
+
   ngOnInit(): void {
     // get the product id from the current route
     const routeParams = this.route.snapshot.paramMap; // access the route parameters using `route.snapshot`
     const productIdFromRoute = Number(routeParams.get('productId'));
 
     // find the product that corresponds with the id provided in route
-    this.product = products.find(product => product.id === productIdFromRoute);
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Boom! Added to the cart! ')
+  }
+
+  
 }
